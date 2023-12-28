3d9cc487710329f51407499a9adb8fa6ec897f35
Ajay <91723540+Ajaynirm@users.noreply.github.com>
2023-12-28 15:25:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ const initGame = () => {
 changeFoodPosition();
 
 // 125 spead of snake
-setIntervalId=setInterval(initGame, 250);
+setIntervalId=setInterval(initGame, 400);
 
 document.addEventListener('keydown',changeDirection);
 
